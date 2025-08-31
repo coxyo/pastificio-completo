9e0b788449fc79fa21429411f8da410dccea1884
coxyo <tua-email@esempio.com>
2025-08-31 21:33:58 +0200

    Fix syntax error in GestoreOrdini

--- pastificio-frontend-final/src/components/GestoreOrdini.js
+++ pastificio-frontend-final/src/components/GestoreOrdini.js
@@ -912,6 +912,10 @@ export default function GestoreOrdini() {
       </Snackbar>
     </Container>
   );
-}/ /   F o r c e   r e b u i l d   0 8 / 3 1 / 2 0 2 5   2 0 : 1 6 : 1 5  
- / /   R e b u i l d   0 8 / 3 1 / 2 0 2 5   2 1 : 2 4 : 5 9  
- 
+}
+
+/ /   F o r c e   r e b u i l d   0 8 / 3 1 / 2 0 2 5   2 0 : 1 6 : 1 5 
+ 
+ / /   R e b u i l d   0 8 / 3 1 / 2 0 2 5   2 1 : 2 4 : 5 9 
+ 
+ 
