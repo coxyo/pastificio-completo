c237639441dd8d2f835b3f6fef7f9259a59b5e86
coxyo <tua-email@esempio.com>
2026-02-24 19:08:46 +0100

    Aggiunta pagina grafici corrispettivi

---
+++ pastificio-frontend-final/app/grafici/page.js
@@ -0,0 +1,8 @@
+// app/grafici/page.js
+'use client';
+
+import GraficiCorrispettivi from '@/components/GraficiCorrispettivi';
+
+export default function GraficiPage() {
+  return <GraficiCorrispettivi />;
+}
