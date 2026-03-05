653baf6196eec68c113a2f9a5195a3561168d515
coxyo <tua-email@esempio.com>
2026-03-05 15:46:01 +0100

    fix: aggiunti Material Icons font per icone MUI su Raspberry Pi

--- pastificio-frontend-final/app/layout.js
+++ pastificio-frontend-final/app/layout.js
@@ -47,6 +47,18 @@ export default function RootLayout({ children }) {
         {/* Preconnect font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        {/* Material Icons - necessario per icone MUI su tutti i dispositivi */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Round"
+          crossOrigin="anonymous"
+        />
+        {/* Material Symbols (icone MUI v5+) */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          crossOrigin="anonymous"
+        />
       </head>
       <body style={{ margin: 0, backgroundColor: '#FDF8F0' }}>
         <ThemeRegistry>
